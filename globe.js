61baa91a46cf7872b3169d43483363dc3ec8c047
Kevin Yoon <jiwhanyoon@college.harvard.edu>
2016-10-29 18:38:54 -0400

    change colors

--- globe.js
+++ globe.js
@@ -192,7 +192,7 @@ DAT.Globe = function(container, opts) {
           lat = data[i];
           lng = data[i + 1];
 //        size = data[i + 2];
-          color = colorFnWrapper(data,i);
+          color = colorFnWrapper(data,i+1);
           console.log('COLOR!!', color);
           size = 0;
           addPoint(lat, lng, size, color, this._baseGeometry);
