42f5c15c1c27ca62488db0071b84ef72c4dc370a
Kevin Yoon <jiwhanyoon@college.harvard.edu>
2016-10-29 20:39:34 -0400

    again

--- globe.js
+++ globe.js
@@ -20,7 +20,7 @@ DAT.Globe = function(container, opts) {
   console.log(opts);
   var colorFn = opts.colorFn || function(x) {
     var c = new THREE.Color();
-    c.setHSL( ( 0.6 - ( x * 0.5 ) ), x * 0.3, 55 );
+    c.setHSL( ( 0.6 - ( x * 0.5 ) ), x * 0.0003, 55 );
     return c;
   };
   var imgDir = opts.imgDir || '/globe/';
