ea8a6a638b5a29f7ba4a721bfcef6cc55746f27a
Kevin Yoon <jiwhanyoon@college.harvard.edu>
2016-10-30 20:50:04 -0400

    new globe image

--- globe.js
+++ globe.js
@@ -108,7 +108,7 @@ DAT.Globe = function(container, opts)
     shader = Shaders['earth'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-    uniforms['texture'].value = THREE.ImageUtils.loadTexture('earth.jpg');
+    uniforms['texture'].value = THREE.ImageUtils.loadTexture('earthmap1k.jpg');
 
     
     
